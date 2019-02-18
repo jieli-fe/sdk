16cbfcf7e182ba7c2d2eb15e578ce36a014d229c
lijie <14734576@qq.com>
2019-02-18 14:21:09 +0800

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,44 +11,7 @@ let prod = merge(baseConfig, {
     },
     optimization: {
         splitChunks:{
-            chunks: 'async',
-            minSize: 30000,
-            maxSize: 0,
-            minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
-            name: true,
-            cacheGroups: {
-                vendors: { // 项目基本框架等
-                    chunks: 'all',
-                    test: /[\\/]node_modules[\\/](react|react-dom|react-dom-router)[\\/]/,
-                    priority: 9,
-                    minSize: 0,
-                    minChunks: 2,
-                    name: 'vendors',
-                },
-                'async-commons': { // 异步加载公共包、组件等
-                    chunks: 'async',
-                    minChunks: 2,
-                    name: 'async-commons',
-                    priority: 8,
-                },
-                commons: { // 其他同步加载公共包
-                    chunks: 'all',
-                    minChunks: 2,
-                    name: 'commons',
-                    priority: 7,
-                },
-                styles: {
-                    name: 'styles',
-                    test: /\.css$/,
-                    chunks: 'all',
-                    priority: 10,
-                    enforce: true,
-                }
-                
-            }
+            chunks: "all"
         },
         minimizer: [
             new UglifyJsPlugin({
